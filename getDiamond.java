c3cce7427255002ee0343b34965b4ad58cf27adc
Jeong_Hyun <105183327+jsjhyun@users.noreply.github.com>
2023-02-02 16:24:09 +0900

    Update getDiamond.java

--- getDiamond.java
+++ getDiamond.java
@@ -1,5 +1,16 @@
 public class Main {
+    public static void getDiamond(int n){
+        for (int i = -n / 2; i <= n/2; i++) {
+            for (int j = 0; j <  Math.abs(i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (n - 2*Math.abs(i)); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-
+        getDiamond(3);
     }
 }
