80a7c192918c2f1878287ce00012555e45d99e29
Jeong_Hyun <105183327+jsjhyun@users.noreply.github.com>
2023-02-02 09:15:37 +0900

    Update 삼각형 .java

--- 삼각형 .java
+++ 삼각형 .java
@@ -1,10 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        for(int i = 1; i <= 3; i++) {
+    public static void getTriangle(int n){
+        for(int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        getTriangle(5);
     }
 }
